757a89d9c9f3e40e3f30141d7060970b23aa2324
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2021-04-14 15:51:53 +0530

    Add files via upload

--- testback/index.js
+++ testback/index.js
@@ -1,39 +1,13 @@
-const express=require("express");
-const app = express();
-/* const port = 3000
+const express = require('express');
 
-app.get('/', (req, res) => res.send('Hello World!'))
-// this is a call back , call back have two parameters (request,responce)
-// types of request:
-// get put delete post requests
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`)) */
+const app=express();
 
-//define a port
-
-const port=8000;
-
-// make a get request
-
-app.get("/login", (req, res) => {
-    return res.send("hello there!you are logged in");
-});
-app.get("/signup", (req,res)=>{
-    return res.send("hello ! you are signed up!");
+const port=3000
+// app.get(route,callback)
+app.get('/',(req,res) => {
+    res.send('Hello World !');
 })
-
-app.get("/hitesh",(req,res)=>{
-    return res.send("he uses instagram !");
+// app.listen(portNumber, callback())
+app.listen(port,() =>{
+    console.log(`listen request at ${port}`);
 })
-app.get("/avinash",(req,res)=>{
-    return res.send("he uses instagram and whatsapp !");
-})
-
-//listen 
-
-app.listen(port,()=>{
-    console.log("server is up and running...");
-});
-
-
-
-// nodemon is used to automatically load the server on saving !
