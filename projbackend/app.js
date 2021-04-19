07194d85b9f1a58335b93607fd2ffbc8efc81337
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2021-04-19 13:19:07 +0530

    Add files via upload

--- projbackend/app.js
+++ projbackend/app.js
@@ -2,9 +2,15 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 const express=require('express');
+const bodyParser=require('body-parser');
+const cookieParser=require('cookie-parser');
+const cors=require('cors');
+
+
+const authRoutes=require('./routes/auth');
 
 const app=express();
-
+const port=process.env.PORT ||8000;
 //mongoose.connect('mongodb://localhost:27017/test')
 
 mongoose.connect(process.env.DATABASE, 
@@ -16,10 +22,15 @@ useCreateIndex:true}).then(() =>{
 .catch((e)=>{
     console.log(e);
 });
+// body parser
+app.use(express.json());
+// cookie parser
+app.use(cookieParser());
+//cors
+app.use(cors());
 
+app.use("/api",authRoutes);
 
-
-const port=process.env.PORT ||8000;
 
 app.listen(port,()=>{
     console.log(`app running on ${port}`);
