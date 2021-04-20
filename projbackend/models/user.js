f2d557cd9436365f8965cb681730dcd0800ea7f7
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2021-04-19 23:40:23 -0700

    Update user.js

--- projbackend/models/user.js
+++ projbackend/models/user.js
@@ -1,85 +1,79 @@
-const mongoose=require('mongoose');
-const cyrpto=require('cyrpto');
-const uuidv1=require('uuid/v1');
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const  uuidv4  = require('uuid/v4');
 
-const userSchema = new mongoose.Schema({
-    name: {
-         type: String,
-         required: true,
-         maxlength:32,
-         trim: true
+  var userSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true,
+        maxlength : 40,
+        trim : true,
     },
-    lastName: {
-        type: String,
-        required: false,
-        maxlength:32,
-        trim: true
+
+    lastname : {
+        type : String,
+        required : false,
+        maxlength : 40,
+        trim : true,
     },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true
+
+    email : {
+        type : String,
+        required : true,
+        trim : true,
+        unique : true,
     },
-    userInfo: {
-        type: String,
-        trim: true
+    
+    encry_password : {
+         type : String,
+         required : true,
     },
-    encry_password: {
-        type: String,
-        required: true
-    },
-    salt: String,
-    role: {
-        type: Number,
-        default:0
+
+
+    salt : String,
+
+    role : {
+        type : Number,
+        default : 0,
+
     },
     purchases : {
-        type: Array,
-        default: []
+        type : Array,
+        default : []
+
+
     }
-
-});
-
-
-//virtuals
-
+     
+    
+}, {timestamps : true});
 
 userSchema.virtual("password")
-          .set(function(password){
-            // _ for private vairable 
-            this._password = password
-            this.salt=uuidv1();
-            this.encry_password = this.securePassword(password)
-          })
-          .get(function(){
-              return this._password
-          })
+    .set(function(password){
+        this._password = password;
+        this.salt =  uuidv4;
+        this.encry_password = this.securePassword(password);
+    })
+    .get(function(){
+        return this.password;
+    })
 
 
-userSchema.method={
 
-    authenticate: function(plainPassword)
-    {
-         return this.securePassword(plainPassword) === this.encry_password
-    },
-    securePassword : function(plainPassword){
-        if(!password)
-        {
-           return "";
+    userSchema.methods = {
+        authetication : function(plainpasswaord){
+            return this.securePassword(plainpasswaord) === this.encry_password
+        },
+
+        securePassword :function(plainpasswaord){
+            if(!plainpasswaord) return "";
+            try{
+                return crypto
+                .createHmac('sha256', this.salt)
+                .update(plainpasswaord)
+                .digest('hex'); 
+            }catch (err) {
+                return "";
+            }
         }
-
-        try
-        {
-            return createHmac('sha256', this.salt)
-            .update(plainPassword)
-            .digest('hex');
-        } 
-        catch(err){
-                  return "";
-        } 
     }
-}
-
-
-module.exports=mongoose.model("User",userSchema);
+module.exports = mongoose.model("User",userSchema)
