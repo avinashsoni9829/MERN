f00f06c1b995100c30447d236bb603ac151310a1
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2021-04-20 12:49:19 +0530

    Add files via upload

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,7 +1,14 @@
 var express = require('express')
 var router = express.Router();
+const { check,validationResult } = require('express-validator');
+const {signout,signup} =require('../controllers/auth')
 
-const {signout} =require('../controllers/auth')
+
+router.post('/signup',[
+    check("name","bname should be atleast 3 characters").isLength({min:3}),
+    check("email","email is required").isEmail(),
+    check("password","password should be atleast 3 characters").isLength({min:3})
+],signup);
 
 router.get("/signout",signout);
 
