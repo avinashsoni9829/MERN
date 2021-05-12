26c92c27b237e7adb486d374a4438bc5565fe23f
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2021-05-12 22:54:21 +0530

    Add files via upload

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,18 +1,28 @@
-var express = require('express')
+var express = require("express");
 var router = express.Router();
-const { check,validationResult } = require('express-validator');
-const {signout,signup} =require('../controllers/auth')
+const { check, validationResult } = require("express-validator");
+const { signout, signup, signin, isSignedIn } = require("../controllers/auth");
+
+router.post(
+  "/signup",
+  [
+    check("name", "name should be at least 3 char").isLength({ min: 3 }),
+    check("email", "email is required").isEmail(),
+    check("password", "password should be at least 3 char").isLength({ min: 3 })
+  ],
+  signup
+);
+
+router.post(
+  "/signin",
+  [
+    check("email", "email is required").isEmail(),
+    check("password", "password field is required").isLength({ min: 1 })
+  ],
+  signin
+);
+
+router.get("/signout", signout);
 
 
-router.post('/signup',[
-    check("name","bname should be atleast 3 characters").isLength({min:3}),
-    check("email","email is required").isEmail(),
-    check("password","password should be atleast 3 characters").isLength({min:3})
-],signup);
-
-router.get("/signout",signout);
-
-
-
-
-module.exports=router;
+module.exports = router;
