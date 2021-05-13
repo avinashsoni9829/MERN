f24b4f54fc1e970f31a4adc96829eea06880fb7b
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2021-05-13 15:15:37 +0530

    Add files via upload

--- projbackend/routes/user.js
+++ projbackend/routes/user.js
@@ -1,7 +1,7 @@
 const express= require('express')
 const router = express.Router();
 
-const  { getUserById, getUser } =require('../controllers/user')
+const  { getUserById, getUser,updateUser,userPurchaseList} =require('../controllers/user')
 const  {isSignedIn,isAuthenticated,isAdmin} =require('../controllers/auth')
 
 
@@ -9,6 +9,14 @@ const  {isSignedIn,isAuthenticated,isAdmin} =require('../controllers/auth')
 router.param("userId",getUserById)
 
 router.get("/user/:userId",  isSignedIn, isAuthenticated,getUser);
+
+// router.get("/users",getAllUsers);
+
+
+router.put("/user/:userId", isSignedIn, isAuthenticated,updateUser);
+
+
+router.get("/order/user/:userId", isSignedIn, isAuthenticated,userPurchaseList);
 
 
 
