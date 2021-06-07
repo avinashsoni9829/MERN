4062931d68a1f8d47f99693c91dcc8bad6a1699c
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2021-06-07 21:16:27 +0530

    Add files via upload

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import AdminRoute from "./auth/helper/AdminRoutes";
+import PrivateRoute from "./auth/helper/PrivateRoutes";
 import Home from "./core/Home";
+import AdminDashBoard from "./user/AdminDashBoard";
 import signin from "./user/Signin";
 import signup from "./user/Signup";
+import UserDashBoard from "./user/UserDashBoard";
 
 const Routes = () => {
   return (
@@ -13,6 +17,12 @@ const Routes = () => {
         <Route path="/signup" exact component={signup} />
 
         <Route path="/signin" exact component={signin} />
+
+        <PrivateRoute path="/user/dashboard" exact component={UserDashBoard} />
+        
+        < AdminRoute path="/admin/dashboard" exact component={AdminDashBoard} />
+
+
       </Switch>
     </BrowserRouter>
   );
