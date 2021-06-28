97a602fd8e6efdeea5d7381b3a4a4b1792d29560
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2021-06-29 00:45:41 +0530

    Add files via upload

--- projbackend/app.js
+++ projbackend/app.js
@@ -39,7 +39,7 @@ app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
 app.use("/api", orderRoutes);
-app.use("/api", paymentBRoutes);
+app.use("/api", paymentBRoutes);  
 //PORT
 const port = process.env.PORT || 8000;
 
