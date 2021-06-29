861b45906c88bb7a3cbab9186ca42cf44909bf1c
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2021-06-29 16:32:31 +0530

    Add files via upload

---
+++ projfrontend/src/user/UserDashBoard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Base from '../core/Base';
+
+const  UserDashBoard = () => {
+    return (
+        <Base title="UserDashBoard Page">
+
+        <h1>this is UserDashBoard page</h1>
+        
+        
+        </Base>
+    )
+}
+
+export default UserDashBoard;
+
+
